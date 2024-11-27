ed13659285d2a17f2f44d73f6d6b21257dd81604
pooja-786 <pjagodara13@gmail.com>
2024-11-27 12:53:25 +0530

    chatapp page

---
+++ index.js
@@ -0,0 +1,14 @@
+//  icon = document.querySelector(".icon");
+// let ul = document.querySelector("ul");
+// let
+// icon.addEventListener("click", () => {
+//     ul.classList.toggle("showData");
+//     console.log(ul);
+
+//     if (ul.className == "showData") {
+//         document.getElementById("bar").className = "fa-solid fa-xmark";
+//     }else{
+//           document.getElementById("bar").className = "fa-solid fa-bars";
+    
+//     }
+// })
